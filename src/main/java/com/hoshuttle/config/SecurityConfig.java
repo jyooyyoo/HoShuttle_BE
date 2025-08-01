f9c316082227a7f2f35d5c17f07ade03733006ce
jyooyyoo <jennyyoo0326@gmail.com>
2025-08-01 14:58:21 +0900

    db 연결 시도

--- src/main/java/com/hoshuttle/config/SecurityConfig.java
+++ src/main/java/com/hoshuttle/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.hoshuttle.backend.config;
+package com.hoshuttle.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,10 +10,6 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable() // CSRF 비활성화
-                .authorizeHttpRequests()
-                .anyRequest().permitAll(); // 모든 요청 허용 (개발 단계)
 
         return http.build();
     }
